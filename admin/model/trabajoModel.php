6e3033e3bf56cd7d36e01f5a3ee9a7dd17fd7b31
Oriol Rivera <siolsiol@hotmail.com>
2013-05-16 23:21:35 -0400

    Update trabajoModel.php

--- admin/model/trabajoModel.php
+++ admin/model/trabajoModel.php
@@ -3,174 +3,124 @@
 
    class trabajo extends Conectar
    {
-
-    public function __construct()
-   	{
+    public function __construct(){
 
       $this->viewPers=array();
       $this->viewEdit=array();
 
-   	}//end construct
+   }//end construct
 
 
 
-   	public function GetPersona(){
+   public function GetPersona(){
 
        parent::con();
-
        $sql=sprintf(
-
              "SELECT id_persona,nombre,nacionalidad,observacion,foto,estado,fecha FROM persona"
-       	);
+             );
 
        $res=mysql_query($sql);
-
        while ($reg=mysql_fetch_assoc($res)) {
-
        	      $this->viewPers[]=$reg;
-
        }
-
-           return $this->viewPers;
-
-
-
+              return $this->viewPers;
+              
    	}//end GetPersona
 
 
-   	public function GetInsert(){
-   		 //print_r($_POST);
+   	public function GetInsert(){ 
 
+         if (empty($_POST["nom_ape"]) or empty($_POST["estado"]))
+	      {
+		
+             	header("Location:".Conectar::ruta()."?accion=add-data&m=1");
+             	exit;
 
-   		         if (empty($_POST["nom_ape"]) or empty($_POST["estado"]))
-
-
-                {
-
-                     	header("Location:".Conectar::ruta()."?accion=add-data&m=1");
-                     	exit;
-
-                }
-
-
-            
-
-
-								$fotJpg=str_replace(".png", ".jpg", $_FILES["foto_per"]["name"]);
-
-
-                                $extImg = pathinfo($fotJpg);
-
-
-                                $ext = array( "jpeg","png","jpg","JPG");
-
-
-					  
+              }
+              
+                  $fotJpg=str_replace(".png", ".jpg", $_FILES["foto_per"]["name"]);
+                  $extImg = pathinfo($fotJpg);
+                  $ext = array( "jpeg","png","jpg","JPG");				  
 
                          	//insert
-
                          	parent::con();
+				$sql=sprintf(
+		                        "INSERT INTO persona
+				         (`id_persona`,
+				         `nombre`,
+				         `apellido`,
+				         `tel_contact`,
+				         `nacionalidad`,
+				         `fecha_nacimiento`,
+				         `cedula_pasaporte`, 
+				         `observacion`, 
+				         `estado`,
+				         `fecha`, 
+				         `responsable`, 
+				         `tel1`, `tel2`)
+				         values(null,%s,%s,%s,%s,%s,%s,%s,%s,now(),%s,%s,%s)",
+					 parent::comillas_inteligentes($_POST["nom_ape"]),
+			                 parent::comillas_inteligentes($_POST["ape_ape"]),
+					 parent::comillas_inteligentes($_POST["tel_con"]),
+			                 parent::comillas_inteligentes($_POST["nac"]),
+			                 parent::comillas_inteligentes($_POST["fec_nac"]),
+			                 parent::comillas_inteligentes($_POST["ced_pas"]),
+					 parent::comillas_inteligentes($_POST["obser"]),
+					 parent::comillas_inteligentes($_POST["estado"]),                                                        
+			                 parent::comillas_inteligentes($_POST["respons"]),
+				         parent::comillas_inteligentes($_POST["tel_res1"]),
+				         parent::comillas_inteligentes($_POST["tel_res2"]));
 
-				              $sql=sprintf(
-
-				                   "INSERT INTO persona
-				                    (`id_persona`, `nombre`, `apellido`, `tel_contact`, `nacionalidad`, `fecha_nacimiento`, `cedula_pasaporte`, `observacion`,  `estado`, `fecha`, `responsable`, `tel1`, `tel2`)
-              									values(
-              									null,
-              									%s,
-              									%s,
-              									%s,
-              									%s,
-                                %s,
-                                %s,
-                                %s,
-                                %s,
-                                now(),
-                                %s,
-                                %s,              									
-                                %s
-
-              									)",
-
-				                    parent::comillas_inteligentes($_POST["nom_ape"]),
-                            parent::comillas_inteligentes($_POST["ape_ape"]),
-				                    parent::comillas_inteligentes($_POST["tel_con"]),
-                            parent::comillas_inteligentes($_POST["nac"]),
-                            parent::comillas_inteligentes($_POST["fec_nac"]),
-                            parent::comillas_inteligentes($_POST["ced_pas"]),
-				                    parent::comillas_inteligentes($_POST["obser"]),
-				                    parent::comillas_inteligentes($_POST["estado"]),                                                        
-                            parent::comillas_inteligentes($_POST["respons"]),
-                            parent::comillas_inteligentes($_POST["tel_res1"]),
-                            parent::comillas_inteligentes($_POST["tel_res2"])
-
-
-				              	);
-
-				              mysql_query($sql);
+				         mysql_query($sql);
 
 				              //*******************
 
 				              //subir foto
-				             $id=mysql_insert_id();
+				         $id=mysql_insert_id();
 
-                        if (!empty($_FILES["foto_per"]["name"])) {
-                            if (array_search($extImg["extension"], $ext))
-                                   {
+                                         if (!empty($_FILES["foto_per"]["name"])) {
+                                                if (array_search($extImg["extension"], $ext)){
 
+				                       $nomp=str_replace(' ', '-',$_POST["nom_ape"]);
 
+				                       copy($_FILES["foto_per"]["tmp_name"], '../public/foto/'.$_FILES["foto_per"]["name"]);
+                                                       
+                                                       $foto=$nomp."-".$id.".jpg";
 
-				              $nomp=str_replace(' ', '-',$_POST["nom_ape"]);
+			                               include("lib/resize-class.php");
+			                               $resizeObj = new resize('../public/foto/'.$_FILES["foto_per"]["name"]);
+          					       $resizeObj -> resizeImage(200, 200, 0);
+          					       $resizeObj -> saveImage('../public/foto/'.$foto, 100);
+          					       unlink('../public/foto/'.$_FILES["foto_per"]["name"]);
 
+  					           //modificamos el registro creado
+  					               $updade=sprintf(
+  					                  "UPDATE persona
+  					                   SET
+  					                   foto=%s
+  					                   WHERE
+  					                   id_persona=%s",
+  					                   parent::comillas_inteligentes($foto),
+  					                   parent::comillas_inteligentes($id)
+  					                 );
+  					                   mysql_query($updade);
 
-                     
-
-
-
-				              copy($_FILES["foto_per"]["tmp_name"], '../public/foto/'.$_FILES["foto_per"]["name"]);
-                              $foto=$nomp."-".$id.".jpg";
-
-
-
-
-
-
-
-
-                              include("lib/resize-class.php");
-                              $resizeObj = new resize('../public/foto/'.$_FILES["foto_per"]["name"]);
-          					          $resizeObj -> resizeImage(200, 200, 0);
-          					          $resizeObj -> saveImage('../public/foto/'.$foto, 100);
-          					          unlink('../public/foto/'.$_FILES["foto_per"]["name"]);
-
-          					           //modificamos el registro creado
-          					            $updade=sprintf(
-          					                "UPDATE persona
-          					                SET
-          					                foto=%s
-          					                WHERE
-          					                id_persona=%s",
-          					                parent::comillas_inteligentes($foto),
-          					                parent::comillas_inteligentes($id)
-          					            );
-          					              mysql_query($updade);
-
-          					             header("Location:".Conectar::ruta()."?accion=edit&data=".base64_encode($id)."&en=".md5('dsfsdfdsfdsfds')."&m=4");
+  					                  header("Location:".Conectar::ruta()."?accion=edit&data=".base64_encode($id)."&en=".md5('dsfsdfdsfdsfds')."&m=4");
 
 
-                                        exit;
+                                                           exit;
 
-                                        }
+                                                     }
                                     
-                                      }
-                                      header("Location:".Conectar::ruta()."?accion=edit&data=".base64_encode($id)."&en=".md5('dsfsdfdsfdsfds')."&m=4");
+                                               }
+                                                  header("Location:".Conectar::ruta()."?accion=edit&data=".base64_encode($id)."&en=".md5('dsfsdfdsfdsfds')."&m=4");
 
                                    
                                    
 
 
 
-   	}//end GetInsert
+          	}//end GetInsert
 
 
     //*********************************************************************
